2d80ff49a048c6a19a7aa382b7698acff5cc7ca3
zyj <edenzyj88@gmail.com>
2024-07-20 14:34:43 +0800

    include all files into git repo

---
+++ implement.py
@@ -0,0 +1,76 @@
+import os
+import glob
+from tika import parser
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+
+database_path = "vectorDB"
+
+def set_vector_db():
+    pdf_dir = 'pdf/starwberry_file/EN'
+    file_names = glob.glob(pdf_dir + "/*.pdf")
+    
+    texts = []
+    
+    for file_name in file_names:
+        text = parser.from_file(file_name)
+        print(type(text["content"]))
+        texts.append(text["content"])
+
+    text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=40)
+
+    chunks = text_splitter.create_documents(texts)
+    print(len(chunks))
+    print(chunks[0])
+
+    embeddings_model = HuggingFaceEmbeddings(
+        model_name = 'sentence-transformers/all-MiniLM-L6-v2',
+        model_kwargs = {'device': 'cuda'},
+        encode_kwargs = {'normalize_embeddings': False}
+    )
+
+    chromadb = Chroma.from_documents(chunks, embeddings_model, persist_directory=database_path)
+    chromadb.persist()
+    
+    return
+
+def retrieve(user_query):
+    print(user_query)
+    
+    embeddings_model = HuggingFaceEmbeddings(
+        model_name = 'sentence-transformers/all-MiniLM-L6-v2',
+        model_kwargs = {'device': 'cuda'},
+        encode_kwargs = {'normalize_embeddings': False}
+    )
+    
+    chromadb = Chroma(embedding_function=embeddings_model, persist_directory=database_path)
+
+    results = chromadb.similarity_search_with_score(user_query, 10)
+    
+    unique_results = set()
+    final_results = []
+
+    for i in range(len(results)):
+        content = results[i][0].page_content
+        if content not in unique_results:
+            unique_results.add(content)
+            final_results.append((content, results[i][1]))
+    
+    final_results.sort(key=lambda a: a[1])
+
+    return_message = user_query
+    
+    if final_results[0][1] <= 0.8:
+        print(final_results[0][1])
+        return_message = return_message + " " + final_results[0][0]
+    
+    # print(type(return_message))
+    # print(return_message)
+    
+    return return_message
+
+# run this python file only when a new vector DB is going to be set up
+if __name__ == "__main__":
+    set_vector_db()
