5365eb09dc4ecd940d3086474cd69a90868d30ec
zyj <edenzyj88@gmail.com>
2024-08-16 15:29:26 +0000

    compare different embedding model

--- experiment-embedding_model.py
+++ experiment-embedding_model.py
@@ -8,6 +8,12 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
+from tart.TART.src.modeling_enc_t5 import EncT5ForSequenceClassification
+from tart.TART.src.tokenization_enc_t5 import EncT5Tokenizer
+import torch
+import torch.nn.functional as F
+import numpy as np
+
 database_path = "vectorDB_test"
 
 def set_vector_db(chunk_size, embedding_model):
@@ -19,11 +25,17 @@ def set_vector_db(chunk_size, embedding_model):
     for file_name in file_names:
         text = parser.from_file(file_name)
         print(type(text["content"]))
-        pdf_str = text["content"].split("References")
+        
+        pdf_str = text["content"]
+        
+        # split References which is not using now.
+        '''pdf_str = pdf_str.split("References")
         for i in range(len(pdf_str) - 1):
             texts.append(pdf_str[i])
         if len(pdf_str) == 1:
-            texts.append(pdf_str[0])
+            texts.append(pdf_str[0])'''
+        
+        texts.append(pdf_str)
 
     text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=40)
 
@@ -42,7 +54,11 @@ def set_vector_db(chunk_size, embedding_model):
         
     os.makedirs(database_path)
 
-    chromadb = Chroma.from_documents(chunks, embeddings_model, persist_directory=database_path)
+    chromadb = Chroma.from_documents(chunks, 
+                                     embedding=embeddings_model,
+                                     collection_name='coll_l2',
+                                     collection_metadata={"hnsw:space": "l2"},
+                                     persist_directory=database_path)
     chromadb.persist()
     
     return len(chunks)
@@ -57,7 +73,10 @@ def retrieve(user_query, num, embedding_model):
         encode_kwargs = {'normalize_embeddings': False}
     )
     
-    chromadb = Chroma(embedding_function=embeddings_model, persist_directory=database_path)
+    chromadb = Chroma(embedding_function=embeddings_model,
+                      collection_name='coll_l2',
+                      collection_metadata={"hnsw:space": "l2"},
+                      persist_directory=database_path)
 
     results = chromadb.similarity_search_with_score(user_query, num)
     
@@ -90,6 +109,79 @@ def retrieve(user_query, num, embedding_model):
     
     return avrg_score
 
+def retrieve_with_re_ranker(user_query, num, embedding_model):
+    embeddings_model = HuggingFaceEmbeddings(
+        model_name = embedding_model,
+        model_kwargs = {'device': 'cuda'},
+        encode_kwargs = {'normalize_embeddings': False}
+    )
+    
+    chromadb = Chroma(embedding_function=embeddings_model,
+                      collection_name='coll_l2',
+                      collection_metadata={"hnsw:space": "l2"},
+                      persist_directory=database_path)
+
+    results = chromadb.similarity_search_with_score(user_query, num)
+    
+    unique_results = set()
+
+    for i in range(len(results)):
+        content = results[i][0].page_content
+        if content not in unique_results:
+            unique_results.add(content)
+    
+    print("number of unique results : {}".format(len(unique_results)))
+    print("=======================")
+    print()
+    
+    model = EncT5ForSequenceClassification.from_pretrained("facebook/tart-full-flan-t5-xl")
+    tokenizer =  EncT5Tokenizer.from_pretrained("facebook/tart-full-flan-t5-xl")
+
+    model.eval()
+    
+    in_answer = "retrieve a passage that answers this question from some paper"
+
+    final_results = []
+    
+    for result in unique_results:
+        final_results.append([result, 0])
+    
+    unique_results = list(unique_results)
+    for i in range(len(unique_results)):
+        for j in range(i+1, len(unique_results)):
+            features = tokenizer(['{0} [SEP] {1}'.format(in_answer, user_query), '{0} [SEP] {1}'.format(in_answer, user_query)], 
+                                 [unique_results[i], unique_results[j]], padding=True, truncation=True, return_tensors="pt")
+            with torch.no_grad():
+                scores = model(**features).logits
+                normalized_scores = [float(score[1]) for score in F.softmax(scores, dim=1)]
+            if np.argmax(normalized_scores) == 0:
+                final_results[i][1] = final_results[i][1] + 1
+            else:
+                final_results[j][1] = final_results[j][1] + 1
+    
+    final_results.sort(reverse=True, key=lambda a: a[1])
+    
+    if len(final_results) < 10:
+        first_num = len(final_results)
+    else:
+        first_num = 10
+
+    result_dir = "results/"
+    result_file = "tart_miniLML6.txt"
+    
+    if os.path.isfile(result_dir+result_file):
+        os.remove(result_dir+result_file)
+    
+    with open(result_dir+result_file, "w") as output_file:
+        for i in range(first_num):
+            output_file.write("Result {} :".format(i))
+            output_file.write("\n")
+            output_file.write(final_results[i][0])
+            output_file.write("\n")
+            output_file.write("\n")
+        
+    return
+
 # run this python file only when a new vector DB is going to be set up
 if __name__ == "__main__":
     user_query = "What is Anthracnose caused by?"
@@ -100,8 +192,11 @@ if __name__ == "__main__":
     chunk_number = set_vector_db(chunk_size, embedding_model)
     
     num = 50
+    
     score = retrieve(user_query, num, embedding_model)
     
     print()
     print("Embedding Model = {} :".format(embedding_model))
     print("average score = {}".format(score))
+        
+    retrieve_with_re_ranker(user_query, num, embedding_model)
