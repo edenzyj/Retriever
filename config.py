a530265d827236b582e2c61c2e03e7ce07493687
zyj <edenzyj88@gmail.com>
2024-12-18 15:51:23 +0800

    refine some bugs after implementation

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # The path of vector DB.
-database_path = "vectorDB"
+database_path = "vectorDB_9907_nFT"
 
 # The path of enterprise data.
 paragraph_directory = 'enterprises/revised_97'
@@ -9,22 +9,22 @@ chunk_size = 200
 
 # Embedding model
 # The embedding model repo from HuggingFace or model path from local.
-embedding_model_path = './finetune_embed/360k_b2/final'
+embedding_model_path = 'dunzhang/stella_en_1.5B_v5'
 # Use finetuned embedding model or not.
-use_finetuned_model = True
+use_finetuned_model = False
 
 # Directory name and file name of query file.
 query_directory = "questions/"
-query_file = "questions_1000.txt"
+query_file = "queries_1000.txt"
 
 # Directory name of both retrieved results file and generated answers file.
 output_directory = "results/97/"
 
 # File name of retrieved results file.
-result_file = "9907_tart_FT_Llama3.2_1000Q_5th_Rtv.json"
+result_file = "9907_tart_nFT_Llama3.2_1000Q_20th_Rtv.json"
 
 # The number of retrieved results merged.
-top_k = 5
+top_k = 20
 
 # Use reranker or not.
 # If use reranker, fill in the model name in string type.
@@ -32,4 +32,4 @@ top_k = 5
 reranker = "facebook/tart-full-flan-t5-xl"
 
 # File name of generated answers file.
-answer_file = "9907_tart_FT_Llama3.2_100Q_5th_Ans.json"
+answer_file = "9907_tart_nFT_Llama3.2_100Q_20th_Ans.json"
--- implement_topK.py
+++ implement_topK.py
@@ -94,8 +94,6 @@ def set_vector_db(file_names, chunk_size, use_finetuned, embedding_model, databa
             encode_kwargs = {'normalize_embeddings': False}
         )
     
-    database_path = database_path + "_{}".format(len(chunks))
-    
     if os.path.isdir(database_path):
         shutil.rmtree(database_path)
         
@@ -247,8 +245,6 @@ if __name__ == "__main__":
     
     chunk_number = set_vector_db(file_names, chunk_size, use_finetuned, embedding_model, database_path)
     
-    database_path = database_path + "_{}".format(chunk_number)
-    
     print("Number of chunks : ".format(chunk_number))
     
     # =====Setting Here=====
@@ -304,8 +300,9 @@ if __name__ == "__main__":
         
         gc.collect()
     
-    with open(result_file, 'w') as output_file:
+    with open(output_file + result_file, 'w') as output_file:
         json.dump(json_results, output_file, indent=4)
+        output_file.close()
     
     '''
     # Seperate retrieving and generating.
@@ -344,3 +341,4 @@ if __name__ == "__main__":
             json_results[i]["answer"] = answer
         
         json.dump(json_results, output_file, indent=4)
+        output_file.close()
